42f510eb2b0d3858b679a85b0f8701c120f87247
Zeyu Xie <xie.zeyu20@gmail.com>
2025-05-03 22:43:55 +0200

    Formatted the codes.

--- src/be.ts
+++ src/be.ts
@@ -1,18 +1,22 @@
-
 // play area
 let ROW_NUM = 4;
 let REEL_NUM = 5;
 
-const SYMBOLS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+const SYMBOLS = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-const PAY_TABLE = Array(9).fill(null).map(() => [0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+const PAY_TABLE = Array(9)
+  .fill(null)
+  .map(() => [
+    0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+    18, 19, 20,
+  ]);
 
-const REEL_1 = [1, 4, 8, 0, 7, 5, 5, 6, 8, 3, 1, 4, 7]
-const REEL_2 = [2, 2, 5, 4, 8, 7, 2, 6, 5, 3, 6, 2, 8]
-const REEL_3 = [1, 5, 6, 0, 2, 0, 1, 4, 2, 3, 8, 1, 7]
-const REEL_4 = [1, 8, 6, 2, 4, 7, 6, 0, 2, 1, 3, 3, 2]
-const REEL_5 = [1, 8, 2, 3, 6, 7, 1, 6, 5, 7, 3, 5, 4]
-const REEL_6 = [6, 5, 8, 6, 2, 3, 3, 6, 0, 2, 5, 1, 8]
+const REEL_1 = [1, 4, 8, 0, 7, 5, 5, 6, 8, 3, 1, 4, 7];
+const REEL_2 = [2, 2, 5, 4, 8, 7, 2, 6, 5, 3, 6, 2, 8];
+const REEL_3 = [1, 5, 6, 0, 2, 0, 1, 4, 2, 3, 8, 1, 7];
+const REEL_4 = [1, 8, 6, 2, 4, 7, 6, 0, 2, 1, 3, 3, 2];
+const REEL_5 = [1, 8, 2, 3, 6, 7, 1, 6, 5, 7, 3, 5, 4];
+const REEL_6 = [6, 5, 8, 6, 2, 3, 3, 6, 0, 2, 5, 1, 8];
 
 const REEL_SET = [REEL_1, REEL_2, REEL_3, REEL_4, REEL_5, REEL_6];
 
@@ -20,13 +24,13 @@ const REEL_SET = [REEL_1, REEL_2, REEL_3, REEL_4, REEL_5, REEL_6];
  * x and y are coordinates
  */
 class Position {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 /** class of win
@@ -35,15 +39,15 @@ class Position {
  * amount: amount of win
  */
 export class Win {
-    symId: number;
-    positions: Position[];
-    amount: number;
+  symId: number;
+  positions: Position[];
+  amount: number;
 
-    constructor(symId: number, positions: Position[], amount: number) {
-        this.symId = symId;
-        this.positions = positions;
-        this.amount = amount;
-    }
+  constructor(symId: number, positions: Position[], amount: number) {
+    this.symId = symId;
+    this.positions = positions;
+    this.amount = amount;
+  }
 }
 
 /** class of spin result
@@ -51,41 +55,41 @@ export class Win {
  * wins: if more than one win conbination occurs at one time, it contains all win conbinations
  */
 class SpinRes {
-    reelStopsFirst: number[]
-    reelStops: number[][]
-    wins: Win[]
+  reelStopsFirst: number[];
+  reelStops: number[][];
+  wins: Win[];
 
-    constructor(reelStopsFirst: number[], reelStops: number[][], wins: Win[]) {
-        this.reelStopsFirst = reelStopsFirst;
-        this.reelStops = reelStops;
-        this.wins = wins;
-    }
+  constructor(reelStopsFirst: number[], reelStops: number[][], wins: Win[]) {
+    this.reelStopsFirst = reelStopsFirst;
+    this.reelStops = reelStops;
+    this.wins = wins;
+  }
 }
 
 // get the all symbols and reels of reel set
 export function GetReelSet(): number[][] {
-    return REEL_SET;
+  return REEL_SET;
 }
 
 // get the number of rows of play area
 export function GetRowNum(): number {
-    return ROW_NUM;
+  return ROW_NUM;
 }
 
 // get the number of reels of play area
 export function GetReelNum(): number {
-    return REEL_NUM;
+  return REEL_NUM;
 }
 
 // set the number of symbols of play area
 export function SetReelNum(value: number) {
-    REEL_NUM = value;
-    // BET_LEVEL = calculateBetLevel(REEL_NUM);    
+  REEL_NUM = value;
+  // BET_LEVEL = calculateBetLevel(REEL_NUM);
 }
 
 // set the number of rows of play area
 export function SetRowNum(value: number) {
-    ROW_NUM = value;
+  ROW_NUM = value;
 }
 
 // get bet amount of every symbol
@@ -96,68 +100,66 @@ export function SetRowNum(value: number) {
 
 // get bet amount of every symbol
 export function GetBetLevel(): number {
-    const base = 10;
-    return REEL_NUM * base;
+  const base = 10;
+  return REEL_NUM * base;
 }
 
 // get spin result
 export function GetRandomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export function GetSpinResult(): SpinRes {
-    const reelStopsFirst = [];
-    const reelStops: number[][] = [];
-    // every reel
-    for (let i = 0; i < REEL_NUM; i++) {
+  const reelStopsFirst = [];
+  const reelStops: number[][] = [];
+  // every reel
+  for (let i = 0; i < REEL_NUM; i++) {
+    const reel = REEL_SET[i];
+    const reelLength = reel.length;
+    const stopIndex = GetRandomInt(0, reel.length);
+    // random index of every symbol of the first row
+    reelStopsFirst.push(stopIndex);
 
-        const reel = REEL_SET[i];
-        const reelLength = reel.length;
-        const stopIndex = GetRandomInt(0, reel.length);
-        // random index of every symbol of the first row
-        reelStopsFirst.push(stopIndex);
-
-        const symbols: number[] = [];
-        for (let j = 0; j < ROW_NUM; j++) {
-            const index = (stopIndex + j) % reelLength;
-            symbols.push(reel[index]);
-        }
-        reelStops.push(symbols);
+    const symbols: number[] = [];
+    for (let j = 0; j < ROW_NUM; j++) {
+      const index = (stopIndex + j) % reelLength;
+      symbols.push(reel[index]);
     }
+    reelStops.push(symbols);
+  }
 
-    return new SpinRes(reelStopsFirst, reelStops, winResults(reelStops));
+  return new SpinRes(reelStopsFirst, reelStops, winResults(reelStops));
 }
 
-
 function winResults(reelStops: number[][]): Win[] {
-    const wins: Win[] = []
-    // every symbol type 
-    for (let symbolId = 0; symbolId < SYMBOLS.length; symbolId++) {
-        let symbolNum = 0;
-        const win = new Win(symbolId, [], 0)
+  const wins: Win[] = [];
+  // every symbol type
+  for (let symbolId = 0; symbolId < SYMBOLS.length; symbolId++) {
+    let symbolNum = 0;
+    const win = new Win(symbolId, [], 0);
 
-        // every reel
-        for (let reelId = 0; reelId < REEL_NUM; reelId++) {
-            const reelStop = reelStops[reelId]
+    // every reel
+    for (let reelId = 0; reelId < REEL_NUM; reelId++) {
+      const reelStop = reelStops[reelId];
 
-            // every symbol of one reel
-            for (let symbolY = 0; symbolY < ROW_NUM; symbolY++) {
-                const symbolIndex = reelStop[symbolY]
-                const symbolPosition = new Position(reelId, symbolY)
-                if (symbolIndex === symbolId) {
-                    symbolNum++
-                    win.positions.push(symbolPosition)
-                }
-            }
+      // every symbol of one reel
+      for (let symbolY = 0; symbolY < ROW_NUM; symbolY++) {
+        const symbolIndex = reelStop[symbolY];
+        const symbolPosition = new Position(reelId, symbolY);
+        if (symbolIndex === symbolId) {
+          symbolNum++;
+          win.positions.push(symbolPosition);
         }
-        win.amount = PAY_TABLE[symbolId][symbolNum]
-        if (win.amount > 0) { wins.push(win) }
+      }
     }
-    return wins
+    win.amount = PAY_TABLE[symbolId][symbolNum];
+    if (win.amount > 0) {
+      wins.push(win);
+    }
+  }
+  return wins;
 }
 
 // ---- test -----
 const spin = GetSpinResult();
 console.log(spin);
-
-
